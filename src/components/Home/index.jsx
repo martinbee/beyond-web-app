d7f76d873b6d12d3d60dbf99e35a403fe69371ba
Martin Bee <martbee88@gmail.com>
2017-08-06 13:57:15 -0500

    feat: add dummy components and structure

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from './Header';
+import WorkoutList from './WorkoutList';
+
+const Home = () => (
+  <div>
+    <Header />
+    <WorkoutList />
+  </div>
+);
+
+export default Home;
