71765b0d76e1ceeb16430315dd92f8ab80ebe225
Martin Bee <martbee88@gmail.com>
2017-08-08 09:18:19 -0500

    feat: add naive business logic that works

--- src/components/Home/WorkoutList/index.jsx
+++ src/components/Home/WorkoutList/index.jsx
@@ -1,55 +1,87 @@
 import React from 'react';
 
+import { coreLifts, weekNumbers } from '../../../constants/';
 import Workout from './Workout';
+
+const trainingMaxes = {
+  press: 120,
+  deadLift: 225,
+  bench: 190,
+  squat: 180,
+};
 
 const workouts = [
   {
-    trainingMax: '190',
     weekNumber: '1',
     coreLift: 'press',
   },
   {
-    trainingMax: '190',
     weekNumber: '1',
     coreLift: 'deadLift',
   },
   {
-    trainingMax: '190',
     weekNumber: '1',
     coreLift: 'bench',
   },
 ];
 
-const coreLifts = ['press', 'deadLift', 'bench', 'squat'];
-const weekNumbers = ['1', '2', '3'];
+const increaseTrainingMax = () => {
+  const { press, deadLift, bench, squat } = trainingMaxes;
+
+  trainingMaxes.press = press + 5;
+  trainingMaxes.bench = bench + 5;
+  trainingMaxes.deadLift = deadLift + 10;
+  trainingMaxes.squat = squat + 10;
+};
+
+// trainingMax must be the last value stored for that lift not just last workout
+const getTrainingMax = (isLastWeekOfCycle, isLastLift, newCoreLift) => {
+  const isTimeToIncreaseTrainingMax = isLastWeekOfCycle && isLastLift;
+
+  if (isTimeToIncreaseTrainingMax) increaseTrainingMax();
+
+  return trainingMaxes[newCoreLift];
+};
+
+const getWeekNumber = (isLastWeekOfCycle, isLastLift, lastWeekNumberIndex) => {
+  if (isLastLift && isLastWeekOfCycle) return weekNumbers[0];
+  if (isLastLift) return weekNumbers[lastWeekNumberIndex + 1];
+
+  return weekNumbers[lastWeekNumberIndex];
+};
+
+const getCoreLift = (isLastLift, lastLiftIndex) => {
+  if (isLastLift) return coreLifts[0];
+
+  return coreLifts[lastLiftIndex + 1];
+};
 
 const WorkoutList = () => {
   const renderWorkouts = () => (
-    workouts.map(Workout)
+    workouts.map(workout => (
+      <Workout {...workout} trainingMax={trainingMaxes[workout.coreLift]} />
+    ))
   );
 
+  // ripe for refactoring into methods
   const addWorkout = () => {
     const lastWorkout = workouts[workouts.length - 1];
-    const lastCoreLift = coreLifts.indexOf(lastWorkout.coreLift);
-    const lastTrainingMax = lastWorkout.trainingMax;
-    const lastWeekNumber = weekNumbers.indexOf(lastWorkout.weekNumber);
+    const { coreLift, weekNumber } = lastWorkout;
 
-    const nextCoreLift = coreLifts[lastCoreLift + 1] || coreLifts[0];
+    const lastLiftIndex = coreLifts.indexOf(coreLift);
+    const isLastLift = lastLiftIndex === coreLifts.length - 1;
 
-    let nextWeekNumber = lastWeekNumber;
+    const lastWeekNumberIndex = weekNumbers.indexOf(weekNumber);
+    const isLastWeekOfCycle = lastWeekNumberIndex === weekNumbers.length - 1;
 
-    if (lastCoreLift === coreLifts.length) {
-      if (lastWeekNumber !== -1) {
-        nextWeekNumber = weekNumbers[lastWeekNumber + 1];
-      } else {
-        nextWeekNumber = weekNumbers[0];
-      }
-    }
+    const newCoreLift = getCoreLift(isLastLift, lastLiftIndex);
+    const newTrainingMax = getTrainingMax(isLastWeekOfCycle, isLastLift, newCoreLift);
+    const newWeekNumber = getWeekNumber(isLastWeekOfCycle, isLastLift, lastWeekNumberIndex);
 
     const newWorkout = {
-      trainingMax: lastTrainingMax,
-      coreLift: nextCoreLift,
-      weekNumber: nextWeekNumber,
+      trainingMax: newTrainingMax,
+      coreLift: newCoreLift,
+      weekNumber: newWeekNumber,
     };
 
     workouts.push(newWorkout);
---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+const coreLifts = ['press', 'deadLift', 'bench', 'squat'];
+const weekNumbers = ['1', '2', '3'];
+
+export { coreLifts, weekNumbers };
